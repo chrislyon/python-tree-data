840b77c990f384a2ddf23fa1d910b60ffcd8df6d
Chris <chris.lyon.tech@gmail.com>
2014-08-08 16:50:51 +0200

    Des test et encore des tests

--- t/t1.py
+++ t/t1.py
@@ -1,8 +1,36 @@
-import shlex
+from tables import *
 
-orig = """
-CREATE MODELE model_name ( id int16, nom S20, no_poste int8 )
-"""
+class Liste_Tel(IsDescription):
+	idn = Int64Col()
+	nom = StringCol(20)
+	tel = StringCol(30)
 
-for l in  shlex.shlex(orig):
-    print l
+
+file_name = 'test.hdf5'
+
+fic = open_file(file_name, mode='w', title = "FICHIER DE TEST")
+
+group = fic.create_group("/", 'data', 'Donnee de test')
+
+t = fic.create_table( group, 'liste', Liste_Tel, "LISTE POSTES SRA")
+
+l = t.row
+l['idn'] = 1
+l['nom'] = 'toto'
+l['tel'] = '0600000000'
+l.append()
+l['idn'] = 2
+l['nom'] = 'titi'
+l['tel'] = '0612000000'
+l.append()
+
+fic.close()
+
+## Now lecture 
+fic = open_file(file_name, mode='w', title = "FICHIER DE TEST")
+
+print fic.root
+
+print fic.root.data.liste
+
+fic.close()
